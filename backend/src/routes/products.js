635c7981d86f2dded4c00ad1e1d78ee9025ffddc
woojongtae <binily@naver.com>
2024-01-13 23:10:33 +0900

    the more

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -2,14 +2,60 @@ const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/auth");
 const Product = require("../models/Product");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
+router.post("/image", auth, (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      // 왜
+      return req.status(500).send(err);
+    }
+    return res.json({ filename: res.req.file.filename });
+  });
+});
 
 router.post("/", auth, async (req, res, next) => {
   try {
-    console.log("여긴오긴하니?");
     const product = new Product(req.body);
-    console.log("gdgd", req.body);
     product.save();
     return res.sendStatus(201);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  const order = req.query.order ? req.query.order : "desc";
+  const sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+  const limit = req.query.limit ? Number(req.query.limit) : 20;
+  const skip = req.query.skip ? Number(req.query.skip) : 0;
+  // const term = req.query.searchTerm;
+
+  try {
+    const products = await Product.find()
+      .populate("writer")
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit);
+    console.log("products", products);
+
+    const productsTotal = await Product.countDocuments();
+    const hasMore = skip + limit < productsTotal ? true : false;
+    return res.status(200).json({
+      products,
+      hasMore,
+    });
   } catch (error) {
     next(error);
   }
